b2783d679001c13d19dfb6ac8125fd3ded2fd2f3
cristina2596 <cosmeci40@gmail.com>
2024-03-05 17:12:34 +0200

    search bar page and bar done

--- src/test/java/pages/SearchBarPage/SearchBarPage.java
+++ src/test/java/pages/SearchBarPage/SearchBarPage.java
@@ -23,6 +23,10 @@ public class SearchBarPage extends BasePage {
 
     private By searchBar = By.id("search");
     private By shakerNegru=By.xpath("//span[contains(.,'Shaker negru 700 ml - GymBeam')]");
+    private By shakerItemPage=By.id("product-addtocart-button");
+    private By itemAddedConfirmationMessage=By.xpath("//div[@class='message-success success message']");
+    private By fierNow=By.xpath("//a[@title='Fier 18 mg - NOW Foods']");
+    private By orderSummaryBox=By.xpath("//tr[@class='totals sub']/th[@class='mark']");
 
     public void setSearchBar(String item1){
         LOG.info("Click 'search bar'");
@@ -32,5 +36,37 @@ public class SearchBarPage extends BasePage {
     public void setShakerNegru(){
         LOG.info("Search 'shaker'");
         driver.findElement(shakerNegru).click();
+    }
+
+    public void addShakerToCart() {
+        LOG.info("Add shaker to cart");
+        driver.findElement(shakerItemPage).click();
+    }
+        public boolean verifyConfirmationMessage(){
+         LOG.info("Verify if item is added to the cart");
+         return driver.findElement(itemAddedConfirmationMessage).isDisplayed();
+        }
+
+    public void searchBarItem2(String item2){
+        LOG.info("Search 'fier") ;
+        driver.findElement(searchBar).click();
+        driver.findElement(searchBar).sendKeys(item2);
+    }
+    public void searchFier(){
+        LOG.info("Fier item page");
+        driver.findElement(fierNow).click();
+    }
+    public void addFierToCart(){
+        LOG.info("Add 'fier' to cart");
+        driver.findElement(shakerItemPage).click();
+
+    }
+    public boolean fierConfirmationMessage(){
+        LOG.info("Fier added to the cart confirmation message");
+        return driver.findElement(itemAddedConfirmationMessage).isDisplayed();
+    }
+    public boolean isSummaryBoxDisplayed(){
+        LOG.info("Verify if order summary box is displayed");
+    return  driver.findElement(orderSummaryBox).isDisplayed();
     }
 }
--- src/test/java/tests/SearchBarTest/SearchBarTest.java
+++ src/test/java/tests/SearchBarTest/SearchBarTest.java
@@ -2,6 +2,7 @@ package tests.SearchBarTest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import tests.BaseTest;
 
@@ -14,7 +15,7 @@ public class SearchBarTest extends BaseTest {
     @Test
     public void searchBarTest() {
         String item1 = "shaker";
-
+        String item2 = "fier";
         sleep(4000);
         LOG.info("Consent");
         registerPage.acceptConsent();
@@ -22,10 +23,42 @@ public class SearchBarTest extends BaseTest {
 
         LOG.info("Search first item");
         searchBarPage.setSearchBar(item1);
-        sleep(1000);
+        sleep(2000);
 
         LOG.info("Search shaker negru");
         searchBarPage.setShakerNegru();
+        sleep(1000);
+
+        LOG.info("Add shaker to cart");
+        searchBarPage.addShakerToCart();
         sleep(3000);
+
+        LOG.info("Verify if product is added to the cart");
+        Assert.assertTrue(searchBarPage.verifyConfirmationMessage(), "Product is added to the cart");
+        sleep(2000);
+
+        LOG.info("Search item 2");
+        searchBarPage.searchBarItem2(item2);
+        sleep(3000);
+
+        LOG.info("Item 2 page");
+        searchBarPage.searchFier();
+        sleep(1000);
+
+        LOG.info("Add item2 to the cart");
+        searchBarPage.addFierToCart();
+        sleep(3000);
+
+        LOG.info("Item 2 added to the cart confirmation message");
+        Assert.assertTrue(searchBarPage.fierConfirmationMessage(),"The item is addded to the cart");
+        sleep(2000);
+
+        LOG.info("Click cart icon");
+        removeProductPage.clickCartIcon();
+        sleep(2000);
+
+        LOG.info("Verify if summary box is displayed");
+        Assert.assertTrue(searchBarPage.isSummaryBoxDisplayed(),"Order summary box is displayed");
+        sleep(2000);
     }
 }
